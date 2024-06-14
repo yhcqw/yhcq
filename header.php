fcc5f58aca769d51feb9f9a88c7ff3b13cee47ad
yhcqw <135196447+yhcqw@users.noreply.github.com>
2024-06-14 16:44:08 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+include 'dbh.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Website Title</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+ 
+    </header>
+
